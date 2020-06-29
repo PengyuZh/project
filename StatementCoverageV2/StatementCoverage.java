bdb92659c3ad90deb35c978d67bb25e0e7ab39e1
PengyuZh <57229565+PengyuZh@users.noreply.github.com>
2020-06-29 03:30:27 +0100

    Create StatementCoverage.java

---
+++ StatementCoverageV2/StatementCoverage.java
@@ -0,0 +1,62 @@
+package org.eclipse.epsilon.eol.coverage;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.xml.bind.Marshaller.Listener;
+
+import org.eclipse.epsilon.common.module.ModuleElement;
+import org.eclipse.epsilon.eol.EolModule;
+import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
+
+import org.eclipse.epsilon.eunit.EUnitModule;
+
+
+public class StatementCoverage {
+
+
+
+	public StatementCoverage(ModuleElement ast, File inputFile) {
+
+	}
+	private static String ParseFile = "src/org/eclipse/epsilon/eol/coverage/my_spider.eol";
+		public static void main(String[] args) {
+		
+		EolModule module = new EolModule();
+		File EolFile = new File(ParseFile);
+		try {
+			module.parse(EolFile);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		EUnitModule eunitModule = new EUnitModule();
+		
+		File eunitFile = new File("src/org/eclipse/epsilon/eol/coverage/test_spider.eunit");
+		try {
+			eunitModule.parse(eunitFile);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		EunitTestCaseListener testCaseListener = new EunitTestCaseListener();
+		eunitModule.addTestListener(testCaseListener);
+				
+		try {
+			eunitModule.execute();
+			
+		} catch (EolRuntimeException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+			
+	}
+
+}
+	
